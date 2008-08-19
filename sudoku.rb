e6da281942c8be0c047c701ad8db6b3b0a2018ef
Gadi Aleksandrowicz <gadial@gadial-desktop.(none)>
2008-08-19 12:03:05 +0300

    Added legality check

--- sudoku.rb
+++ sudoku.rb
@@ -6,6 +6,15 @@ class Integer
 	end
 end
 
+class Array
+	def has_duplicates?
+		self.uniq.length < self.length
+	end
+	def find_all_indexes
+		(0...self.length).find_all{|i| yield(self[i])}
+	end
+end
+
 class Class
   def delegate_methods(instance_var, *methods)
 #     puts "Delegating methods %p to variable %p" % [methods, instance_var]
@@ -21,13 +30,19 @@ class SudokuBoard
 	$BoardBasicSize=3
 	$BoardSize=$BoardBasicSize**2 #The assumption is that $BoardSize=n^2 for some positive n
 		def initialize
-		@squares=DoubleArray.new($BoardSize,$BoardSize)
+		@basic_squares=DoubleArray.new($BoardSize,$BoardSize)
 	end
 	def row(index)
 		(0...$BoardSize).collect{|j| self[index,j]}
 	end
 	def rows
 		(0...$BoardSize).collect{|i| row(i)}
+	end
+	def columns
+		(0...$BoardSize).collect{|i| column(i)}
+	end
+	def squares
+		(0...$BoardSize).collect{|i| square(i)}
 	end
 	def column(index)
 		(0...$BoardSize).collect{|i| self[i,index]}
@@ -40,12 +55,15 @@ class SudokuBoard
 			self[row_num+offset_row, col_num+offset_col]
 		end
 	end
-        delegate_methods :squares, '[]=', '[]'
+        delegate_methods :basic_squares, '[]=', '[]'
 	def each_row
 		(0...$BoardSize).times {|i| yield(row(i),i)}
 	end
 	def each_column
 		(0...$BoardSize).times {|i| yield(column(i),i)}
+	end
+	def each_square
+		(0...$BoardSize).times {|i| yield(square(i),i)}
 	end
 	def inspect_row(row)
 		row.collect{|x| (x != nil)?(x.to_s):("#")}.join(" ")+"\n"
@@ -63,6 +81,18 @@ class SudokuBoard
 		end
 		self
 	end
+	def illegal(vector)
+		(vector-[nil]).has_duplicates?
+	end
+	def illegals
+		rows.find_all_indexes{|row| illegal(row)}.collect{|i| [:row,i]} +
+		columns.find_all_indexes{|column| illegal(column)}.collect{|i| [:column,i]}+
+		squares.find_all_indexes{|square| illegal(square)}.collect{|i| [:square,i]}
+	end
+
+	def legal?
+		illegals.empty?
+	end
 
 	def compress
 		rows.collect{|row| row.collect{|x| (x!=nil)?(x.to_s):("0")}}.flatten.join("")
@@ -73,7 +103,10 @@ class SudokuBoard
 end
 
 temp=SudokuBoard.load("test.sud")
+temp[0,7]=4
 puts temp.inspect
+puts temp.legal?
+puts temp.illegals.inspect
 # temp=SudokuBoard.new
 # temp[3,5]=5
 # temp[2,2]=2
